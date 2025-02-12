04b5ad6cd866087e772b5ea3a3547045532cbcd5
PremaNandika <nandika183@gmail.com>
2025-02-12 10:54:49 +0800

    :sparkles: first commit :sparkles:

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="dark:text-white">Welcome to SvelteKit</h1>
+<p class="dark:text-white">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
