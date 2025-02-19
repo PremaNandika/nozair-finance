8b940c660bfa12d59a1c6849a02dbf489062ffd0
PremaNandika <nandika183@gmail.com>
2025-02-19 22:04:40 +0800

    style: change homepage style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,11 +10,15 @@
 	/>
 </svelte:head>
 <div class="container flex h-[80vh] flex-col items-center justify-center">
-	<Card.Root class="mx-auto max-w-3xl border-2">
-		<Card.Header>
-			<Card.Title class="text-center font-atkinson text-4xl">Nozair Finance Tracker</Card.Title>
+	<Card.Root class="mx-auto max-w-3xl rounded-none border-x-0 border-y-2">
+		<Card.Header class="p-4 pb-0 md:p-6 md:pb-0 ">
+			<Card.Title class="text-center font-atkinson text-3xl md:text-4xl"
+				>Nozair Finance Tracker</Card.Title
+			>
 		</Card.Header>
-		<Card.Content class="flex flex-col space-y-6 text-center font-atkinson text-sm">
+		<Card.Content
+			class="flex flex-col space-y-4 p-4 text-center font-atkinson text-xs md:space-y-6 md:p-6 md:text-sm"
+		>
 			<p>
 				Managing money should be simple and stress-free. Our upcoming app will help you track income
 				and expenses effortlessly, set budgets in real-time, and gain clear insights into your
